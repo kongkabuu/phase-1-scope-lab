354a87d5692f1e6e541ef566572cd591ff22dc8b
paul kabuu <kongkabuu9@gmail.com>
2023-03-22 12:40:11 +0300

    completed assingmnet

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your solution in this file!
+var customerName = 'bob'
+
+console.log(customerName);
+function upperCaseCustomerName() {
+    customerName = 'bob'.toUpperCase();
+}
+function setBestCustomer() {
+    return bestCustomer = 'not bob';
+}
+function overwriteBestCustomer() {
+    return bestCustomer = 'maybe bob';
+}
+const leastFavoriteCustomer = 'Zio'
+function changeLeastFavoriteCustomer() {
+     leastFavoriteCustomer = 'Zio';
+}
